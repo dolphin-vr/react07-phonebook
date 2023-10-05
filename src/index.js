e5795347d98364e23eac5009fc554a650632fdd7
dolphin <fun53@ukr.net>
2023-10-05 23:41:49 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = {
   colors: {
@@ -26,9 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
           <App />
-        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
